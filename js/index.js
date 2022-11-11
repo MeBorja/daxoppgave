c72762b28a023ee15d9497bdc49d8706ad74673c
MeBorja <93922493+MeBorja@users.noreply.github.com>
2022-11-11 14:58:43 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,100 @@
+console.log("Shit works")
+const emailSignup = document.getElementById("floatingInputSignUp")
+const passSignup = document.getElementById("floatingPasswordSignUp")
+const passSignin = document.getElementById("floatingPassword-signin")
+const emailSignin = document.getElementById("floatingInput-signin")
+const nummberChange = document.querySelector(".numberChange")
+const plus = document.querySelector(".plus")
+const minus = document.querySelector(".minus")
+
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore, collection, onSnapshot,
+    addDoc, deleteDoc, doc,
+    updateDoc
+} from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4krVZ0LaMmvzBfuDCjJYso6LkjIgIaNY",
+    authDomain: "daxoppgave2.firebaseapp.com",
+    projectId: "daxoppgave2",
+    storageBucket: "daxoppgave2.appspot.com",
+    messagingSenderId: "622167249135",
+    appId: "1:622167249135:web:db19ae6488848a41634b1a"
+  };
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore()
+
+const colRef = collection(db, `login`)
+
+const number = collection(db, `number`)
+
+
+
+onSnapshot(number, (snapshot) => {
+    let currentnumb = []
+    snapshot.docs.forEach((doc) =>{
+        currentnumb.push({...doc.data(), id: doc.id})
+    })
+    nummberChange.innerHTML = `${currentnumb[0].nummberValue}`
+  })
+plus.addEventListener('click', (e) => {
+    console.log("lmao")
+    const update = doc(db, "number", "gzeyGptBTv9PoPig7vHv")
+    onSnapshot(number, (snapshot) => {
+        let currentnumb = []
+        snapshot.docs.forEach((doc) =>{
+            currentnumb.push({...doc.data(), id: doc.id})
+            nummberChange.innerHTML = `${currentnumb[0].nummberValue}`
+            currentnumb.value + 1;
+            updateDoc(update, {
+                number: `${currentnumb.value}`
+            })
+        })
+      })
+    })
+minus.addEventListener('click', (e) => {
+    console.log("lmao")
+})
+
+
+const addUser = document.querySelector(".sign-up")
+addUser.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log("User was added")
+    addDoc(colRef, {
+        email: emailSignup.value,
+        pass: passSignup.value
+    })
+    .then(() => {
+        addUser.value = "";
+    })
+})
+console.log("Shit works")
+onSnapshot(colRef, (snapshot) => {
+    let login = []
+    snapshot.docs.forEach((doc) =>{
+        login.push({...doc.data(), id: doc.id})
+    })
+    console.log(login)
+  })
+
+const loginUser = document.querySelector(".sign-in")
+loginUser.addEventListener("submit", (e) => {
+    e.preventDefault()
+    onSnapshot(colRef, (snapshot) => {
+        let login = []
+        snapshot.docs.forEach((doc) =>{
+            login.push({...doc.data(), id: doc.id})
+        })
+        console.log("Works")
+        login.forEach((item) => {
+            if (item.email == emailSignin.value && item.pass === passSignin.value) {
+                console.log("SHIT FUCKING WORKS!!!")
+                console.log(item.email, "has logged in!!")
+            }
+        })
+      })
+})
